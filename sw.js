8de3794eac63c20a3a600673ef1cba2e34953228
chrnvshk <daria.che02@gmail.com>
2025-06-11 11:18:09 +0000

    ➕ Add service worker (PWA support)

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('sleep-widget-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/favicon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});

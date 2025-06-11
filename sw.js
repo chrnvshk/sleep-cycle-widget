15c3cccb5badefbbee19339651da5960468c60ee
chrnvshk <daria.che02@gmail.com>
2025-06-11 12:09:56 +0000

    refactor: оновлено service worker з кешуванням і skipWaiting

--- sw.js
+++ sw.js
@@ -1,18 +1,39 @@
+const CACHE_NAME = 'sleep-widget-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/133.png'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('sleep-widget-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/favicon.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
